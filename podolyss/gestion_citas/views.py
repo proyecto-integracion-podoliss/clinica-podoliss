fa75fba028bae3c5343a0e257ade387e1bfa6716
BastianPerezG <bastianesteban.perez@gmail.com>
2024-11-18 22:16:18 -0300

    puliendo algunas cosas, agregando el alfa de la landing

--- podolyss/gestion_citas/views.py
+++ podolyss/gestion_citas/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+
+class LandingPageView(TemplateView):
+    template_name = "landing_page.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['titulo'] = "Bienvenido a Podolyss"
+        context['caracteristicas'] = [
+            "Gestión de pacientes y profesionales",
+            "Agendamiento intuitivo",
+            "Notificaciones y recordatorios",
+        ]
+        return context
